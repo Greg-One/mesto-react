8094c6ce20ccbc236a0c5fa8c5c2d8e3cc4eace6
Grigory <sgmdoc@yandex.ru>
2021-01-28 10:20:55 +0500

    feat: add initial markup & utils

--- src/App.js
+++ src/App.js
@@ -3,21 +3,245 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="page">
+      <header className="header">
+        <img
+          src="./images/logo.svg"
+          alt="Логотип проекта 'Место'"
+          className="header__logo"
+        />
       </header>
+
+      <main>
+        <section className="profile">
+          <div className="profile__main">
+            <button
+              aria-label="Изменить аватар"
+              className="profile__avatar-edit"
+            >
+              <img
+                src="./images/Cousteau.jpg"
+                alt="Аватар пользователя"
+                className="profile__avatar"
+              />
+            </button>
+            <div className="profile__info">
+              <div className="profile__user">
+                <h1 className="profile__name">Жак-Ив Кусто</h1>
+                <p className="profile__occupation">Исследователь океана</p>
+              </div>
+              <button
+                type="button"
+                aria-label="Редактировать профиль"
+                className="profile__edit-button"
+              ></button>
+            </div>
+          </div>
+          <button
+            type="button"
+            aria-label="Добавить фотографию"
+            className="profile__add-button"
+          ></button>
+        </section>
+
+        <section className="cards"></section>
+      </main>
+
+      <footer className="footer">
+        <p className="footer__copyright">&copy; 2020 Mesto Russia</p>
+      </footer>
+
+      <section className="popup popup_profile">
+        <form
+          name="profile-info"
+          className="popup__container popup__container_profile"
+          novalidate
+        >
+          <button
+            type="button"
+            aria-label="Закрыть попап редактирования профиля"
+            className="popup__close-button"
+          ></button>
+          <h2 className="popup__title">Редактировать профиль</h2>
+          <fieldset name="user" className="popup__info">
+            <input
+              type="text"
+              placeholder="Имя"
+              name="name"
+              required=""
+              minlength="2"
+              maxlength="40"
+              className="popup__input popup__input_type_name"
+              id="profile-name-input"
+            />
+            <span
+              className="popup__input-error"
+              id="profile-name-input-error"
+            ></span>
+            <input
+              type="text"
+              placeholder="Род занятий"
+              name="job"
+              required=""
+              minlength="2"
+              maxlength="200"
+              className="popup__input popup__input_type_occupation"
+              id="profie-occupation-input"
+            />
+            <span
+              className="popup__input-error"
+              id="profie-occupation-input-error"
+            ></span>
+          </fieldset>
+          <button
+            aria-label="Сохранить новые данные профиля"
+            className="popup__submit-button"
+          >
+            Сохранить
+          </button>
+        </form>
+      </section>
+
+      <section className="popup popup_card">
+        <form
+          name="add-card"
+          className="popup__container popup__container_card"
+          novalidate
+        >
+          <button
+            type="button"
+            aria-label="Закрыть попап добавления фотографии"
+            className="popup__close-button"
+          ></button>
+          <h2 className="popup__title">Новое место</h2>
+          <fieldset name="card" className="popup__info">
+            <input
+              type="text"
+              placeholder="Название"
+              name="name"
+              required=""
+              minlength="2"
+              maxlength="30"
+              className="popup__input popup__input_type_title"
+              id="card-title-input"
+            />
+            <span
+              className="popup__input-error"
+              id="card-title-input-error"
+            ></span>
+            <input
+              type="url"
+              placeholder="Ссылка на изображение"
+              name="link"
+              required=""
+              className="popup__input popup__input_type_url"
+              id="card-url-input"
+            />
+            <span
+              className="popup__input-error"
+              id="card-url-input-error"
+            ></span>
+          </fieldset>
+          <button
+            aria-label="Создать новую карточку с фотографией"
+            className="popup__submit-button"
+          >
+            Создать
+          </button>
+        </form>
+      </section>
+
+      <section className="popup popup_image">
+        <div className="popup__image-container">
+          <button
+            type="button"
+            aria-label="Закрыть попап с фотографией"
+            className="popup__close-button"
+          ></button>
+          <img
+            src="#"
+            alt="Фотография выбранного места"
+            className="popup__photo"
+          />
+          <h3 className="popup__card-title">Название выбранного места</h3>
+        </div>
+      </section>
+
+      <section className="popup popup_remove">
+        <form className="popup__container popup__container_remove">
+          <h2 className="popup__title popup__title_remove">Вы уверены?</h2>
+          <button
+            aria-label="Подтвердить удаление карточки"
+            className="popup__submit-button popup__submit-button_remove"
+          >
+            Да
+          </button>
+          <button
+            type="button"
+            aria-label="Закрыть окно удаления карточки"
+            className="popup__close-button"
+          ></button>
+        </form>
+      </section>
+
+      <section className="popup popup_avatar">
+        <form
+          name="change-avatar"
+          className="popup__container popup__container_avatar"
+          novalidate
+        >
+          <button
+            type="button"
+            aria-label="Закрыть окно изменения аватара"
+            className="popup__close-button"
+          ></button>
+          <h2 className="popup__title">Обновить аватар</h2>
+          <fieldset name="avatar" className="popup__info">
+            <input
+              type="url"
+              placeholder="Ссылка на изображение"
+              name="link"
+              required=""
+              className="popup__input popup__input_avatar popup__input_type_url"
+              id="avatar-url-input"
+            />
+            <span
+              className="popup__input-error"
+              id="avatar-url-input-error"
+            ></span>
+          </fieldset>
+          <button
+            aria-label="Сохранить новый аватар"
+            className="popup__submit-button"
+          >
+            Сохранить
+          </button>
+        </form>
+      </section>
+
+      <template className="card-template">
+        <article className="card">
+          <button
+            type="button"
+            className="card__remove-button"
+            aria-label="Удалить карточку"
+          ></button>
+          <div className="card__image-container">
+            <img src="#" alt="Фотография места" className="card__image" />
+          </div>
+          <div className="card__description">
+            <h2 className="card__title"></h2>
+            <div className="card__like-container">
+              <button
+                type="button"
+                aria-label="Поставить карточке лайк"
+                className="card__like-button"
+              ></button>
+              <p className="card__like-count">0</p>
+            </div>
+          </div>
+        </article>
+      </template>
     </div>
   );
 }
