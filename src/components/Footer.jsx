dc0b5d4cfc727e44a921261b283946be472d6254
Greg-one <sgmdoc@yandex.ru>
2021-01-28 22:47:27 +0500

    feat: remove excess imports

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import '../index.css';
-
 function Footer() {
   return (
     <footer className="footer">
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import '../index.css';
 import avatar from '../images/Cousteau.jpg';
 
 function handleEditAvatarClick() {
