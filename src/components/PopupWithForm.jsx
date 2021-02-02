2b907b54a20e4a31dea63373fff9a138c8003ae9
Greg-one <sgmdoc@yandex.ru>
2021-02-02 23:00:09 +0500

    feat: add page overflow: hidden

--- src/components/PopupWithForm.jsx
+++ src/components/PopupWithForm.jsx
@@ -15,7 +15,9 @@ function PopupWithForm(props) {
           onClick={props.onClose}
         ></button>
         <h2 className="popup__title">{props.title}</h2>
+
         {props.children}
+
         <button aria-label="Сохранить" className="popup__submit-button">
           {props.buttonText}
         </button>
