9694af9d79c7f062e6bf47a1dc0305fc83864ae9
Grigory <sgmdoc@yandex.ru>
2021-02-12 08:48:19 +0500

    feat: add EditProfilePopup component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ import PopupWithForm from './PopupWithForm.jsx';
 import ImagePopup from './ImagePopup.jsx';
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 import api from '../utils/Api.js';
+import EditProfilePopup from './EditProfilePopup.jsx';
 
 function App() {
   {
@@ -73,46 +74,6 @@ function App() {
           onCardClick={handleCardClick}
         />
         <Footer />
-
-        {/* Попап редактирования профиля */}
-        <PopupWithForm
-          name="profile"
-          title="Редактировать профиль"
-          buttonText="Сохранить"
-          isOpen={isEditProfilePopupOpen}
-          onClose={closeAllPopups}
-        >
-          <fieldset name="user" className="popup__info">
-            <input
-              type="text"
-              placeholder="Имя"
-              name="name"
-              required=""
-              minLength="2"
-              maxLength="40"
-              className="popup__input popup__input_type_name"
-              id="profile-name-input"
-            />
-            <span
-              className="popup__input-error"
-              id="profile-name-input-error"
-            ></span>
-            <input
-              type="text"
-              placeholder="Род занятий"
-              name="job"
-              required=""
-              minLength="2"
-              maxLength="200"
-              className="popup__input popup__input_type_occupation"
-              id="profie-occupation-input"
-            />
-            <span
-              className="popup__input-error"
-              id="profie-occupation-input-error"
-            ></span>
-          </fieldset>
-        </PopupWithForm>
 
         {/* Попап добавления карточки */}
         <PopupWithForm
---
+++ src/components/EditProfilePopup.jsx
@@ -0,0 +1,44 @@
+function EditProfilePopup(props) {
+  return (
+    <PopupWithForm
+      name="profile"
+      title="Редактировать профиль"
+      buttonText="Сохранить"
+      isOpen={isEditProfilePopupOpen}
+      onClose={closeAllPopups}
+    >
+      <fieldset name="user" className="popup__info">
+        <input
+          type="text"
+          placeholder="Имя"
+          name="name"
+          required=""
+          minLength="2"
+          maxLength="40"
+          className="popup__input popup__input_type_name"
+          id="profile-name-input"
+        />
+        <span
+          className="popup__input-error"
+          id="profile-name-input-error"
+        ></span>
+        <input
+          type="text"
+          placeholder="Род занятий"
+          name="job"
+          required=""
+          minLength="2"
+          maxLength="200"
+          className="popup__input popup__input_type_occupation"
+          id="profie-occupation-input"
+        />
+        <span
+          className="popup__input-error"
+          id="profie-occupation-input-error"
+        ></span>
+      </fieldset>
+    </PopupWithForm>
+  );
+}
+
+export default EditProfilePopup;
